65e609c9eeb19a3b7ad91a47b51341ceed8e7533
Carlos <102881005+daitanboy@users.noreply.github.com>
2025-05-11 11:04:23 -0400

    Update prepare_data.py

--- src/data/prepare_data.py
+++ src/data/prepare_data.py
@@ -1,22 +1,7 @@
-"""
-prepare_data.py
-Handles loading and cleaning Kaggle and Twitter datasets.
-"""
-
-import pandas as pd
-import re
-
+# Clean text (lowercasing, remove special characters)
 def clean_text(text):
-    text = text.lower()
-    text = re.sub(r'http\S+', '', text)
-    text = re.sub(r'@\w+', '', text)
+    text = str(text).lower()
+    text = re.sub(r"http\S+|www\S+", '', text)
+    text = re.sub(r'@\w+|#\w+', '', text)
     text = re.sub(r'[^a-zA-Z\s]', '', text)
     return text
-
-def load_kaggle_data(fake_path, true_path):
-    df_fake = pd.read_csv(fake_path)
-    df_true = pd.read_csv(true_path)
-    df_fake['label'] = 0
-    df_true['label'] = 1
-    df_combined = pd.concat([df_fake, df_true], ignore_index=True)
-    return df_combined
